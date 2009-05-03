61c8f8f702b5ab0f1bf0c6ca42c731a4aa8ef778
Beer Lee <beer@pixnet.tw>
2009-05-03 23:47:52 +0800

    test branch

--- blog.js
+++ blog.js
@@ -3,4 +3,5 @@ jQuery(function() {
     var mlink = jQuery('<a>');
     mlink.attr('href', 'http://blog.beerlee88.com/blog/murmur').text(murmur);
     jQuery('.murmur-text').empty().append(mlink);
+
 });
