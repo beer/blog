e5b70aa02af4e25ac41a5c3f7a010c08b524cbdf
Beer Lee <beer@pixnet.tw>
2009-05-03 23:43:37 +0800

    update source

--- blog.js
+++ blog.js
@@ -1,1 +1,6 @@
-/net/account/admin/beer/WWW/beer/blog.js
+jQuery(function() {
+    var murmur = jQuery('.murmur-text').text();
+    var mlink = jQuery('<a>');
+    mlink.attr('href', 'http://blog.beerlee88.com/blog/murmur').text(murmur);
+    jQuery('.murmur-text').empty().append(mlink);
+});
