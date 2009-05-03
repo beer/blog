b1739cd51ec152f97d78eaa68210ea0d6132bd47
Beer Lee <beer@pixnet.tw>
2009-05-04 01:07:42 +0800

    murmur list add padding & width

--- blog.js
+++ blog.js
@@ -3,5 +3,4 @@ jQuery(function() {
     var mlink = jQuery('<a>');
     mlink.attr('href', 'http://blog.beerlee88.com/blog/murmur').text(murmur);
     jQuery('.murmur-text').empty().append(mlink);
-
 });
